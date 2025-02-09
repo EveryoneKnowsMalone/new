0fcd6c23a6b2386385999f902a15d2943e7c9523
EveryoneKnowsMalone <aguiyiebube123@gmail.com>
2025-02-09 10:02:34 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -23,21 +23,3 @@ searchInput.addEventListener('input', (event) => {
         card.style.display = title.includes(query) ? 'block' : 'none';
     });
 });
-
-// Load user data from Telegram WebApp
-window.Telegram.WebApp.ready(); // Initialize the web app
-
-const userData = window.Telegram.WebApp.initDataUnsafe;
-
-// Extract user information
-const userId = userData.user.id;
-const userName = userData.user.username || 'Anonymous';
-const userPhoto = userData.user.photo_url || 'default-photo.png';
-const userBio = userData.user.bio || 'No bio provided';
-
-console.log(userId, userName, userPhoto, userBio);
-
- // Inject user data into the profile page
-  document.getElementById('profile-pic').src = userData.user.photo_url || 'default-photo.png';
-  document.getElementById('username').innerText = `@${userData.user.username || 'Anonymous'}`;
-  document.getElementById('bio').innerText = userData.user.bio || 'No bio provided';
